96cfa54917abbc70a9c68f31767cae8adc8bdfd9
Golshid Bahadorian <36138378+golshidb93@users.noreply.github.com>
2018-02-06 23:04:59 +0100

    Develop (#1)
    * ..
    
    * Adds a new person class. To be used for further development in the future.

---
+++ src/main/java/com/example/golshidsdemo/Person.java
@@ -0,0 +1,13 @@
+package com.example.golshidsdemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private int age;
+}
+
+
